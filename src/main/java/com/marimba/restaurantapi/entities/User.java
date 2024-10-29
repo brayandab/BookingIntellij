bc0681c8927aa66451c5551cd54b01ba1331650e
brayan04123 <brayandavidbanguera@gmail.com>
2024-10-28 22:07:33 -0500

    Actualizacion User

--- src/main/java/com/marimba/restaurantapi/entities/User.java
+++ src/main/java/com/marimba/restaurantapi/entities/User.java
@@ -6,6 +6,7 @@ import java.util.List;
 @Entity
 @Table(name = "USERS")
 public class User {
+    //segunda prueba
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID", unique = true, nullable = false)
